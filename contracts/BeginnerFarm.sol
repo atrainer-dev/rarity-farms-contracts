1bd6e082520d4136ef2774a4118c384a7055046e
Andrew Throener <athroener@gmail.com>
2021-09-14 00:37:09 -0500

    major overhaul

--- contracts/BeginnerFarm.sol
+++ contracts/BeginnerFarm.sol
@@ -35,6 +35,7 @@ contract BeginnerFarm is ERC20 {
   }
 
   function farmCorn(uint256 summoner) public {
+    // May do something with level here.  leval 1-5 get 1 per day.  5-10 get 2.  idk yet.
     // Figure out how to do this once per day
     // Similar to adventure function.  https://ftmscan.com/address/0xce761d788df608bd21bdd59d6f4b54b2e27f25bb#code
     corn.mint(summoner, 1 * 1e18);
